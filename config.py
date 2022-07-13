d71589ac98d17aec12646cb602b865dce056d1ad
Martin Scharfenberg <martinmarx2@gmail.com>
2022-07-13 15:40:51 +0200

    added config docu

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 """
+Configuration constants
 
+CRYPT_STORE_PATH: str
+    full pyth of store file i.e. "/home/user/store"
+BIND_ADDRESS: str
+    http listen address i.e. "0.0.0.0" or "127.0.0.1"
+BIND_PORT: int
+    http listen port ie. 8000
 """
 CRYPT_STORE_PATH = "/home/user/store"
 BIND_ADDRESS = "0.0.0.0"
