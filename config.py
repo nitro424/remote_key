01c9ae3d97c1d1e1961c767c0e0a921551b3c893
Martin Scharfenberg <martinmarx2@gmail.com>
2022-07-13 19:36:56 +0200

    load env vars to config

--- config.py
+++ config.py
@@ -11,3 +11,11 @@ BIND_PORT: int
 CRYPT_STORE_PATH = "/home/user/store"
 BIND_ADDRESS = "0.0.0.0"
 BIND_PORT = 8000
+
+
+# Env vars overrite config
+import os
+
+CRYPT_STORE_PATH = os.environ.get("CRYPT_STORE_PATH") or CRYPT_STORE_PATH
+BIND_ADDRESS = os.environ.get("BIND_ADDRESS") or BIND_ADDRESS
+BIND_PORT = os.environ.get("BIND_PORT") or BIND_PORT
